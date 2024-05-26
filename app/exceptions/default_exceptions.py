6a24386a41df66224c9f0ee7fd680696c1980def
UdsonWillams <udson.willams@gmail.com>
2024-05-26 00:07:05 -0300

    exceptions

--- app/exceptions/default_exceptions.py
+++ app/exceptions/default_exceptions.py
@@ -1,7 +1,4 @@
-from typing import (
-    Any,
-    Dict,
-)
+from typing import Any, Dict
 
 from fastapi import HTTPException, status
 
@@ -15,11 +12,32 @@ class DefaultApiException(HTTPException):
     ) -> None:
         super().__init__(status_code, detail, headers)
 
+
 class InternalServerErrorException(DefaultApiException):
     def __init__(
         self,
         status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
         detail: Any = {"error": "Some error ocurred!"},
+        headers: Dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
+
+
+class ApiInvalidResponseException(DefaultApiException):
+    def __init__(
+        self,
+        status_code: int = 403,
+        detail: Any = {"error": "Invalid values for the api"},
+        headers: Dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
+
+
+class MongoRepositoryTransactionsException(DefaultApiException):
+    def __init__(
+        self,
+        status_code: int = 500,
+        detail: Any = {"error": "Invalid transaction in mongoDB"},
         headers: Dict[str, str] | None = None,
     ) -> None:
         super().__init__(status_code, detail, headers)
